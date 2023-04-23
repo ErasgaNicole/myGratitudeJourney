3958356add8e181230872c130394c0350f66ea59
ErasgaNicole <erasganicole@yahoo.com>
2023-04-23 16:34:38 +0800

    Added notesTakerActivity and XML

--- app/src/main/java/com/example/mygratitudejourney/notesTakerActivity.java
+++ app/src/main/java/com/example/mygratitudejourney/notesTakerActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class notesTakerActivity extends AppCompatActivity {
-
+public class notesTakerActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_taker);
     }
